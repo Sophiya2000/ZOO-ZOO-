1794d079def8f93b8047548a960dce890aca333d
Sophiya <102750844+Sophiya2000@users.noreply.github.com>
2022-04-02 19:13:14 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const navi = document.getElementById("add");
+const n2 = document.getElementById("card");
+function a1() {
+    alert("not aviable");
+}
+navi.addEventListener("click", a1);
+n2.addEventListener("click", a1);
